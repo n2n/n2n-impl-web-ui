735f27c68a5657af232d998baa09781ef36a8a87
Andreas von Burg <von-burg@hnm.ch>
2025-07-19 18:39:33 +0200

    $imgAttrs

--- src/app/n2n/impl/web/ui/view/html/HtmlBuilder.php
+++ src/app/n2n/impl/web/ui/view/html/HtmlBuilder.php
@@ -53,7 +53,7 @@ class HtmlBuilder {
 		$this->meta = new HtmlBuilderMeta($view);
 		$this->contentBuffer = $contentBuffer;
 	}
-	
+
 	/**
 	 * @return \n2n\impl\web\ui\view\html\HtmlBuilderMeta
 	 */
@@ -69,39 +69,39 @@ class HtmlBuilder {
 	}
 
 	/**
-	 * 
+	 *
 	 */
 	public function headContents() {
 		$htmlProperties = $this->meta->getHtmlProperties();
-		
+
 		if (!$htmlProperties->containsName(HtmlBuilderMeta::HEAD_TITLE_KEY)) {
 			$htmlProperties->set(HtmlBuilderMeta::HEAD_TITLE_KEY, new HtmlElement('title', null,
 					N2N::getAppConfig()->general()->getPageName()));
 		}
-		
+
 		if (!$this->contentBuffer->hasBreakPoint(HtmlBuilderMeta::HEAD_TITLE_KEY)) {
 			$this->contentBuffer->breakPoint(HtmlBuilderMeta::HEAD_TITLE_KEY);
 		}
-		
+
 		if (!$this->contentBuffer->hasBreakPoint(HtmlBuilderMeta::HEAD_META_KEY)) {
 			$this->contentBuffer->breakPoint(HtmlBuilderMeta::HEAD_META_KEY);
 		}
-		
+
 		if (!$this->contentBuffer->hasBreakPoint(HtmlBuilderMeta::HEAD_LINK_KEY)) {
 			$this->contentBuffer->breakPoint(HtmlBuilderMeta::HEAD_LINK_KEY);
 		}
-		
+
 		if (!$this->contentBuffer->hasBreakPoint(HtmlBuilderMeta::HEAD_SCRIPT_KEY)) {
 			$this->contentBuffer->breakPoint(HtmlBuilderMeta::HEAD_SCRIPT_KEY);
 		}
-		
+
 		if (!$this->contentBuffer->hasBreakPoint(HtmlBuilderMeta::HEAD_CONTENTS_KEY)) {
 			$this->contentBuffer->breakPoint(HtmlBuilderMeta::HEAD_CONTENTS_KEY);
 		}
 	}
 
 	/**
-	 * 
+	 *
 	 */
 	public function headEnd() {
 		$this->headContents();
@@ -118,21 +118,21 @@ class HtmlBuilder {
 	}
 
 	/**
-	 * 
+	 *
 	 */
 	public function bodyStartContents() {
 		$this->contentBuffer->breakPoint(HtmlBuilderMeta::TARGET_BODY_START);
 	}
 
 	/**
-	 * 
+	 *
 	 */
 	public function bodyEndContents() {
 		$this->contentBuffer->breakPoint(HtmlBuilderMeta::TARGET_BODY_END);
 	}
 
 	/**
-	 * 
+	 *
 	 */
 	public function bodyEnd() {
 		$this->bodyEndContents();
@@ -149,30 +149,30 @@ class HtmlBuilder {
 	public function out($contents) {
 		$this->view->out($this->getOut($contents));
 	}
-	
+
 	public function getOut($contents) {
 		if ($contents instanceof UiComponent) {
 			return $contents;
 		}
-		
+
 		return $this->getEsc($contents);
 	}
-	
+
 // 	public function getUic($arg) {
 // 		if ($arg instanceof UiComponent) {
 // 			return $arg;
 // 		}
-		
+
 // 		return new Raw($arg);
 // 	}
-	
+
 	/**
 	 * @param mixed $contents
 	 */
 	public function esc($contents) {
 		$this->view->out($this->getEsc($contents));
 	}
-	
+
 	/**
 	 * @param mixed $contents
 	 * @return \n2n\web\ui\Raw
@@ -194,13 +194,13 @@ class HtmlBuilder {
 	 * @return \n2n\web\ui\Raw
 	 */
 	public function getEscP($contents, ?array $attrs = null, bool $strict = false): UiComponent {
-	    $attrsHtml = HtmlElement::buildAttrsHtml($attrs);
+		$attrsHtml = HtmlElement::buildAttrsHtml($attrs);
 		$html = HtmlUtils::escape($contents, function ($html) use ($strict, $attrsHtml) {
 			$html = str_replace("\n\n", '</p><p' . $attrsHtml . '>', str_replace("\r", '', $html));
 			if (!$strict) $html = nl2br($html);
 			return $html;
 		});
-		
+
 		return new Raw('<p' . $attrsHtml . '>' . $html . '</p>');
 	}
 	/**
@@ -217,10 +217,10 @@ class HtmlBuilder {
 		$html = HtmlUtils::escape($contents, function ($html) {
 			return nl2br($html);
 		});
-		
+
 		return new Raw($html);
 	}
-	
+
 	/*
 	 * NAVIGATION UTILS
 	 */
@@ -251,28 +251,28 @@ class HtmlBuilder {
 			$murl = $this->view->buildUrlStr($murl, $required, $suggestedLabel);
 			$label = $this->meta->buildLinkLabel($murl, $suggestedLabel);
 		}
-		
+
 		$raw = new HtmlSnippet();
 		$raw->append($this->getLinkStart($murl, $attrs, $alternateTagName, $alternateAttrs, $required));
 		$raw->append($label);
 		$raw->append($this->getLinkEnd());
-		
+
 		return $raw;
 	}
-	
+
 	private $linkStartedData = null;
-	
+
 	private function ensureLinkStarted() {
 		if ($this->linkStartedData === null) {
 			throw new UiException('No Link started.');
 		}
 	}
-	
+
 	public function linkStart($murl, ?array $attrs = null, ?string $alternateTagName = null,
 			?array $alternateAttrs = null, bool $required = false) {
 		$this->view->out($this->getLinkStart($murl, $attrs, $alternateTagName, $alternateAttrs, $required));
 	}
-	
+
 	/**
 	 * @param array $attrs
 	 * @return array
@@ -283,66 +283,66 @@ class HtmlBuilder {
 		}
 		return $attrs;
 	}
-	
+
 	public function getLinkStart($murl, ?array $attrs = null, ?string $alternateTagName = null,
 			?array $alternateAttrs = null, bool $required = false) {
 		if ($this->linkStartedData !== null) {
 			throw new UiException('Link already started.');
 		}
-		
+
 		$href = null;
 		$suggestedLabel = null;
 		if ($murl !== null) {
 			$href = $this->view->buildUrlStr($murl, $required, $suggestedLabel);
 		}
-		
+
 		$attrs = $this->secureLinkAttrs((array) $attrs);
-		
+
 		if ($href !== null) {
 			$this->linkStartedData = array('tagNameHtml' => 'a', 'href' =>  $href, 'suggestedLabel' => $suggestedLabel);
 			$attrs = HtmlUtils::mergeAttrs(array('href' =>  $href), $attrs);
 			return new Raw('<a ' . HtmlElement::buildAttrsHtml($attrs) . '>');
-		} 
-		
+		}
+
 		if ($alternateTagName !== null) {
 			$this->linkStartedData = array('tagNameHtml' => $this->getEsc($alternateTagName));
 		} else {
 			$this->linkStartedData = array('tagNameHtml' => 'a');
 		}
-		
-		return new Raw('<' . $this->linkStartedData['tagNameHtml'] . ' ' 
+
+		return new Raw('<' . $this->linkStartedData['tagNameHtml'] . ' '
 				. HtmlElement::buildAttrsHtml($alternateAttrs ?? $attrs) . '>');
 	}
-	
+
 	public function linkLabel($label = null) {
 		$this->view->out($this->getLinkLabel($label));
 	}
-	
+
 	public function getLinkLabel($label = null) {
 		$this->ensureLinkStarted();
-		
+
 		if ($label !== null) {
 			return $this->getOut($label);
 		}
-		
+
 		if (isset($this->linkStartedData['suggestedLabel'])) {
 			return $this->getOut($this->linkStartedData['suggestedLabel']);
 		}
-		
-		if (isset($this->linkStartedData['href'])) { 
+
+		if (isset($this->linkStartedData['href'])) {
 			return $this->getOut($this->meta->buildLinkLabel($this->linkStartedData['href']));
 		}
-		
+
 		return null;
 	}
-	
+
 	public function linkEnd() {
 		$this->view->out($this->getLinkEnd());
 	}
-	
+
 	public function getLinkEnd() {
 		$this->ensureLinkStarted();
-		
+
 		$raw = new Raw('</' . $this->linkStartedData['tagNameHtml'] . '>');
 		$this->linkStartedData = null;
 		return $raw;
@@ -374,19 +374,19 @@ class HtmlBuilder {
 	 */
 	public function getLinkToContext($pathExt, $label, ?array $attrs = null, $query = null,
 			$fragment = null, $ssl = null, $subsystem = null) {
-		return new Link($this->meta->getContextUrl($pathExt, $query, $fragment, $ssl, $subsystem), 
+		return new Link($this->meta->getContextUrl($pathExt, $query, $fragment, $ssl, $subsystem),
 				$label, $attrs);
 	}
 
 	public function linkToController($pathExt, $label, ?array $attrs = null, $query = null,
 			$fragment = null, $contextKey = null, $ssl = null, $subsystem = null) {
-		$this->view->out($this->getLinkToController($pathExt, $label, $attrs, $query, $fragment, 
+		$this->view->out($this->getLinkToController($pathExt, $label, $attrs, $query, $fragment,
 				$contextKey, $ssl, $subsystem));
 	}
-	
+
 	public function getLinkToController($pathExt, $label, ?array $attrs = null, $query = null, $fragment = null,
 			$contextKey = null, $ssl = null, $subsystem = null) {
-		return new Link($this->meta->getControllerUrl($pathExt, $query, $fragment, $contextKey, $ssl, $subsystem), 
+		return new Link($this->meta->getControllerUrl($pathExt, $query, $fragment, $contextKey, $ssl, $subsystem),
 				$label, $attrs);
 	}
 
@@ -409,9 +409,9 @@ class HtmlBuilder {
 			$fragment = null, $ssl = null, $subsystem = null) {
 		return new Link($this->meta->getUrl($pathExt, $query, $fragment, $ssl, $subsystem), $label, $attrs);
 	}
-	
+
 	/**
-	 * 
+	 *
 	 * @param string $email
 	 * @param string|UiComponent $label
 	 * @param array $attrs
@@ -435,7 +435,7 @@ class HtmlBuilder {
 				new Raw('</a>'));
 	}
 	/**
-	 * 
+	 *
 	 * @param string $email
 	 * @param string|UiComponent $label
 	 * @param array $attrs
@@ -443,8 +443,8 @@ class HtmlBuilder {
 	public function linkTel(string $tel, $label = null, ?array $attrs = null) {
 		$this->view->out($this->getLinkTel($tel, $label, $attrs));
 	}
-	
-	/** 
+
+	/**
 	 * @param string $email
 	 * @param string|UiComponent $label
 	 * @param array $attrs
@@ -454,16 +454,16 @@ class HtmlBuilder {
 		HtmlUtils::validateCustomAttrs((array) $attrs, array('href'));
 
 		return new HtmlSnippet(
-				new Raw('<a href="tel:' . preg_replace('/[^0-9\\+]/', '', str_replace('(0)', '', $tel)) . '"' 
+				new Raw('<a href="tel:' . preg_replace('/[^0-9\\+]/', '', str_replace('(0)', '', $tel)) . '"'
 						. HtmlElement::buildAttrsHtml($attrs) . '>'),
 				($label !== null ? $label : new Raw($tel)),
 				new Raw('</a>'));
 	}
-	
+
 	/*
-	 * MESSAGE CONTAINER UTILS 
+	 * MESSAGE CONTAINER UTILS
 	 */
-	
+
 	/**
 	 * @param string $groupName
 	 * @param int $severity
@@ -475,10 +475,10 @@ class HtmlBuilder {
 	 */
 	public function messageList(?string $groupName = null, ?int $severity = null, ?array $attrs = null, ?array $errorAttrs = null,
 			?array $warnAttrs = null, ?array $infoAttrs = null, ?array $successAttrs = null) {
-		$this->view->out($this->getMessageList($groupName, $severity, $attrs, $errorAttrs, 
+		$this->view->out($this->getMessageList($groupName, $severity, $attrs, $errorAttrs,
 				$warnAttrs, $infoAttrs, $successAttrs));
 	}
-	
+
 	/**
 	 * @param string $groupName
 	 * @param int $severity
@@ -491,16 +491,16 @@ class HtmlBuilder {
 	 */
 	public function getMessageList(?string $groupName = null, ?int $severity = null, ?array $attrs = null, ?array $errorAttrs = null,
 			?array $warnAttrs = null, ?array $infoAttrs = null, ?array $successAttrs = null) {
-		
-		return new MessageList($this->view->getDynamicTextCollection(), 
-				$this->meta->getMessages($groupName, $severity), $attrs, 
+
+		return new MessageList($this->view->getDynamicTextCollection(),
+				$this->meta->getMessages($groupName, $severity), $attrs,
 				$errorAttrs, $warnAttrs, $infoAttrs, $successAttrs);
 	}
-	
+
 	/*
-	 * L10N UTILS 
+	 * L10N UTILS
 	 */
-	
+
 	/**
 	 * @param string $code
 	 * @param array $args
@@ -512,7 +512,7 @@ class HtmlBuilder {
 			$module = null) {
 		$this->l10nText($code, $args, $num, $replacements, $module);
 	}
-	
+
 	/**
 	 * @param string $code
 	 * @param array $args
@@ -525,7 +525,7 @@ class HtmlBuilder {
 			$module = null) {
 		return $this->getL10nText($code, $args, $num, $replacements, $module);
 	}
-		
+
 	/**
 	 * @param string $key
 	 * @param array $args
@@ -537,17 +537,17 @@ class HtmlBuilder {
 	public function getL10nText(string $key, ?array $args = null, ?int $num = null, ?array $replacements = null, $module = null) {
 		$textRaw = $this->getEsc($this->view->getL10nText($key, $args, $num, null, $module));
 		if (empty($replacements)) return $textRaw;
-		
+
 // 		$textHtml = (string) $textRaw;
 // 		foreach ($replacements as $key => $replacement) {
-// 			$textHtml = str_replace(DynamicTextCollection::REPLACEMENT_PREFIX . $key . DynamicTextCollection::REPLACEMENT_SUFFIX, 
+// 			$textHtml = str_replace(DynamicTextCollection::REPLACEMENT_PREFIX . $key . DynamicTextCollection::REPLACEMENT_SUFFIX,
 // 					HtmlUtils::contentsToHtml($replacement, $this->view->getContentsBuildContext()), $textHtml);
 // 		}
 
 		$textHtml = HtmlBuilderMeta::replace((string) $textRaw, $replacements, $this->view);
 		return new Raw($textHtml);
 	}
-	
+
 	/**
 	 * @param string $key
 	 * @param array $args
@@ -559,15 +559,15 @@ class HtmlBuilder {
 	public function l10nText(string $key, ?array $args = null, ?int $num = null, ?array $replacements = null, $module = null) {
 		$this->view->out($this->getL10nText($key, $args, $num, $replacements, $module));
 	}
-	
+
 	public function getL10nNumber($value, $style = \NumberFormatter::DECIMAL, $pattern = null) {
 		return $this->getEsc($this->view->getL10nNumber($value, $style, $pattern));
 	}
-	
+
 	public function l10nNumber($value, $style = \NumberFormatter::DECIMAL, $pattern = null) {
 		$this->view->out($this->getL10nNumber($value, $style, $pattern));
 	}
-	
+
 	/**
 	 * @see self::getL10nCurrency()
 	 * @return \n2n\web\ui\Raw
@@ -575,7 +575,7 @@ class HtmlBuilder {
 	public function getL10nCurrency($value, $currency = null) {
 		return $this->getEsc($this->view->getL10nCurrency($value, $currency));
 	}
-	
+
 	/**
 	 * @param float $value
 	 * @param string $currency The 3-letter ISO 4217 currency code indicating the currency to use.
@@ -583,61 +583,62 @@ class HtmlBuilder {
 	public function l10nCurrency($value, $currency = null) {
 		$this->view->out($this->getL10nCurrency($value, $currency));
 	}
-	
+
 	public function getL10nDate($value, $dateStyle = null, ?\DateTimeZone $timeZone = null) {
 		return $this->getEsc($this->view->getL10nDate($value, $dateStyle, $timeZone));
 	}
-	
+
 	public function l10nDate($value, $dateStyle = null, ?\DateTimeZone $timeZone = null) {
 		$this->view->out($this->getL10nDate($value, $dateStyle, $timeZone));
 	}
-	
+
 	public function getL10nTime($value, $timeStyle = null, ?\DateTimeZone $timeZone = null) {
 		return $this->getEsc($this->view->getL10nTime($value, $timeStyle, $timeZone));
 	}
-	
+
 	public function l10nTime($value, $timeStyle = null, ?\DateTimeZone $timeZone = null) {
 		$this->view->out($this->getL10nTime($value, $timeStyle, $timeZone));
 	}
-	
+
 	public function getL10nDateTime($value, $dateStyle = null, $timeStyle = null, ?\DateTimeZone $timeZone = null) {
 		return $this->getEsc($this->view->getL10nDateTime($value, $dateStyle, $timeStyle, $timeZone));
 	}
-	
+
 	public function l10nDateTime(?\DateTime $value = null, $dateStyle = null, $timeStyle = null, ?\DateTimeZone $timeZone = null) {
 		$this->view->out($this->getL10nDateTime($value, $dateStyle, $timeStyle, $timeZone));
 	}
-	
+
 	public function getL10nDateTimeFormat(?\DateTime $dateTime, string $icuPattern, ?\DateTimeZone $timeZone = null) {
 		return $this->getEsc($this->view->getL10nDateTimeFormat($dateTime, $icuPattern, $timeZone));
 	}
-	
+
 	public function l10nDateTimeFormat(?\DateTime $dateTime, string $icuPattern, ?\DateTimeZone $timeZone = null) {
 		$this->view->out($this->getL10nDateTimeFormat($dateTime, $icuPattern, $timeZone));
 	}
-	
+
 	/*
 	 * IMAGE UTILS
 	 */
-	
+
 	public function image(?File $file = null, $imgComposer = null, ?array $attrs = null,
-			bool $attrWidth = true, bool $attrHeight = true) {
-		$this->view->out($this->getImage($file, $imgComposer, $attrs, $attrWidth, $attrHeight));
+			bool $attrWidth = true, bool $attrHeight = true, array $imgAttrs = null) {
+		$this->view->out($this->getImage($file, $imgComposer, $attrs, $attrWidth, $attrHeight, $imgAttrs));
 	}
 
 
 
 	public function getImage(?File $file = null, $imgComposer = null, ?array $attrs = null,
-			bool $addWidthAttr = true, bool $addHeightAttr = true) {
+			bool $addWidthAttr = true, bool $addHeightAttr = true, array $imgAttrs = null) {
 		ArgUtils::valType($imgComposer, array(ImgComposer::class, ThumbStrategy::class), true);
 
 		if ($imgComposer instanceof ImgComposer) {
 			$imgSet = $imgComposer->createImgSet($file, $this->view->getN2nContext());
 
 			if (!$imgSet->isPictureRequired()) {
-				return UiComponentFactory::createImg($imgSet, $attrs, $addWidthAttr, $addHeightAttr);
+				return UiComponentFactory::createImg($imgSet, HtmlUtils::mergeAttrs((array) $attrs, $imgAttrs, true),
+						$addWidthAttr, $addHeightAttr);
 			} else {
-				$imgAttrs = [];
+				$imgAttrs ??= $imgAttrs;
 
 				if (isset($attrs['alt'])) {
 					$imgAttrs['alt'] = $attrs['alt'];
@@ -655,47 +656,47 @@ class HtmlBuilder {
 
 		return $this->getImg($file, $imgComposer, $attrs, $addWidthAttr, $addHeightAttr);
 	}
-	
+
 	public function getImg(?File $file = null, $imgComposer = null, ?array $attrs = null,
 			bool $addWidthAttr = true, bool $addHeightAttr = true) {
 		ArgUtils::valType($imgComposer, array(ImgComposer::class, ThumbStrategy::class), true);
-		
+
 		if ($imgComposer instanceof ImgComposer) {
 			$imgSet = $imgComposer->createImgSet($file);
-				
+
 			if ($imgSet->isPictureRequired()) {
 				throw new CouldNotRenderUiComponentException('ImgComposer requires picture element.');
-			} 
-			
+			}
+
 			return UiComponentFactory::createImg($imgSet, $attrs, $addWidthAttr, $addHeightAttr);
 		}
-		
+
 		return UiComponentFactory::createImgFromThSt($file, $imgComposer, $attrs, $addWidthAttr, $addHeightAttr);
 	}
-	
+
 	public function getPicture(?File $file = null, ?ImgComposer $imgComposer = null, ?array $attrs = null) {
 		if ($imgComposer === null) {
 			return UiComponentFactory::createPicture($imgComposer, $attrs);
 		}
-				
-		return UiComponentFactory::createPicture($imgComposer->createImgSet($file, $this->view->getN2nContext()), 
+
+		return UiComponentFactory::createPicture($imgComposer->createImgSet($file, $this->view->getN2nContext()),
 				$attrs);
 	}
-	
-	
+
+
 	public function imageAsset($pathExt, $alt, ?array $attrs = null, ?string $moduleNamespace = null) {
 		$this->view->out($this->getImageAsset($pathExt, $alt, $attrs, $moduleNamespace));
 	}
-	
+
 	public function getImageAsset($pathExt, $alt, ?array $attrs = null, ?string $moduleNamespace = null) {
 		if ($moduleNamespace === null) {
 			$moduleNamespace = $this->view->getModuleNamespace();
 		}
 		return new HtmlElement('img', HtmlUtils::mergeAttrs(
-				array('src' => $this->view->getHttpContext()->getAssetsUrl($moduleNamespace)->ext($pathExt), 
+				array('src' => $this->view->getHttpContext()->getAssetsUrl($moduleNamespace)->ext($pathExt),
 						'alt' => $alt), (array) $attrs));
 	}
-	
+
 	/**
 	 * @param mixed $pathExt
 	 * @param mixed $imgComposer
@@ -708,7 +709,7 @@ class HtmlBuilder {
 			bool $addWidthAttr = true, bool $addHeightAttr = true) {
 		$this->out($this->getImageMimgAsset($pathExt, $imgComposer, $attrs, $moduleNamespace, $addWidthAttr, $addHeightAttr));
 	}
-	
+
 	/**
 	 * @param mixed $pathExt
 	 * @param mixed $imgComposer
@@ -722,17 +723,17 @@ class HtmlBuilder {
 			bool $addWidthAttr = true, bool $addHeightAttr = true) {
 		$assetFileManager = $this->view->lookup(AssetFileManager::class);
 		CastUtils::assertTrue($assetFileManager instanceof AssetFileManager);
-		
+
 		if ($moduleNamespace === null) {
 			$moduleNamespace = $this->view->getModuleNamespace();
 		}
-		
+
 		$qnLevels = [VarStore::namespaceToDirName($moduleNamespace)];
 		array_push($qnLevels, ...Path::create($pathExt)->getPathParts());
 		$fileName = array_pop($qnLevels);
-		
+
 		$file = $assetFileManager->getByQualifiedName((new QualifiedNameBuilder($qnLevels, $fileName))->__toString());
-		
+
 		return $this->getImage($file, $imgComposer, $attrs, $addWidthAttr, $addHeightAttr);
 	}
 }
